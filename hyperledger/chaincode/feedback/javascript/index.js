43ce558da0ce8d36f7c331af80256fc029a0d85f
Paritosh Panda <paritoshpanda21@gmail.com>
2023-11-27 19:42:03 +0530

    linting changes

--- hyperledger/chaincode/feedback/javascript/index.js
+++ hyperledger/chaincode/feedback/javascript/index.js
@@ -2,9 +2,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-'use strict';
+"use strict";
 
-const feedback = require('./lib/feedback');
+const feedback = require("./lib/feedback");
 
 module.exports.feedback = feedback;
-module.exports.contracts = [ feedback ];
+module.exports.contracts = [feedback];
