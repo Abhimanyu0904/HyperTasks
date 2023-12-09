c3e129860292c69cc2dbcada5772d70bb83c836b
Abhimanyu0904 <abhimanyu.sharma_ug23@ashoka.edu.in>
2023-12-09 13:33:18 +0530

    app.py and templates updated

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,6 @@
 from flask import render_template, url_for, redirect, flash, request, session
-# from project1 import db, app, bcrypt, images
 from forms import *
-# from .models import *
-# from flask_login import login_user, current_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, LoginManager, UserMixin
 
 
 """
@@ -18,7 +16,17 @@ from flask import Flask
 ########## GLOBAL VARIABLES ##########
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Please login first to use your cart"
+login_manager.login_message_category = 'warning'
+
+
 app.config['SECRET_KEY'] = "feedback"
+
+NODE_PATH = "usr/bin/node"
+FABRIC_PATH = "/Users/abhimanyu_sharma/Desktop/Ashoka_Monsoon_23/Blockchain_Project/feedback/hyperledger/feedback/javascript"
 
 ########## CHAINCODE PROCESSOR ##########
 def chaincode(args: List[str]):
@@ -44,6 +52,21 @@ def chaincode(args: List[str]):
 
     return valid, out
 
+class User(UserMixin):
+    def __init__(self, email, password, user_type):
+        self.id = email
+        self.password = password
+        self.user_type = user_type
+
+@login_manager.user_loader
+def load_user(user_id):
+    # valid, user_data = chaincode(["getUser", user_id, password])
+    # if valid and user_data:
+    #     user_type = user_data['type']
+    #     return User(user_id, password, user_type)
+    # return None
+    pass
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -58,7 +81,19 @@ def register():
     registration_form = RegistrationForm()
     if request.method == 'POST':
         if registration_form.validate_on_submit():
-            return render_template("register.html",form=registration_form)
+            ashokaid = registration_form.ashokaid.data
+            email_id = registration_form.email_id.data
+            name = registration_form.name.data
+            type = registration_form.type.data
+            valid, output = chaincode([name, email_id, ashokaid, type])
+            if valid:
+                #TODO: Parse Output after chaincode execution
+                flash("Registration Request Sent Successfully. You will receive an email notification when your request is processed.", "success")
+                return redirect(url_for('register'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
+                return redirect(url_for('register'))
+            # return render_template("register.html",form=registration_form)
     return render_template("register.html",form=registration_form)
 
 @app.route("/login", methods = ['GET', 'POST'])
@@ -66,55 +101,192 @@ def login():
     login_form = LoginForm()
     if request.method == 'POST':
         if login_form.validate_on_submit():
+            email_id = login_form.email_id.data
+            password = login_form.password.data
+            valid, output = chaincode(["queryUser",email_id, password])
+            if valid:
+                #TODO: Parse Output after chaincode execution
+                session['email_id'] = email_id
+                session['password'] = password
+
+                session['type'] = output.type #change this later when we know how output looks like
+                flash("Login Successful", "success")
+                return redirect(url_for('display_requests'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
             return render_template("login.html",form=login_form)
     return render_template("login.html",form=login_form)
 
+@app.route("/admin_dashboard")
+def admin_dashboard():
+    return render_template("admin_dashboard.html")
 
 @app.route("/admin_login", methods = ['GET', 'POST'])
 def admin_login():
     admin_login_form = AdminLoginForm()
+    if 'admin_password' in session:
+        if request.method == 'POST':
+            if admin_login_form.validate_on_submit():
+                password = admin_login_form.password.data
+                valid, output = chaincode([password])
+                #Have to add code to validate admin login credentials
+
+                #if validated
+                session['admin_password'] = password
+
+                return render_template("admin_login.html",form=admin_login_form)
+        return render_template("admin_login.html",form=admin_login_form)
+    else:
+        flash("You have to be an admin to access this page. Please login first", "danger")
+        return redirect(url_for('admin_login'))
+@app.route("/user_registration_requests", methods = ['GET', 'POST'])
+def user_registration_requests():
+    accept_user_registration_request_form = AcceptUserRegistrationRequestForm()
+    reject_user_registration_request_form = RejectUserRegistrationRequestForm()
+    return render_template("user_registration_requests.html", accept_user_registration_request_form = accept_user_registration_request_form, reject_user_registration_request_form = reject_user_registration_request_form)
+
+@app.route("/admin_display_requests", methods = ['GET', 'POST'])
+def admin_display_requests():
+    initiate_request_form = InitiateRequestForm()
+    hold_request_form = HoldRequestForm()
+    resume_request_form = ResumeRequestForm()
+    finish_request_form = FinishRequestForm()
+    drop_request_form = DropRequestForm()
+    if request.method == "POST" and 'initiate' in request.form:
+        if initiate_request_form.validate_on_submit():
+            request_id = initiate_request_form.request_id.data
+            valid, output = chaincode(["updateRequest", request_id, "Initiating Request","in progress"])
+            if valid:
+                flash("Request Initiated!", "success")
+                return redirect(url_for('admin_display_requests'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
+                return redirect(url_for('admin_display_requests'))
+    elif request.method == "POST" and 'finish' in request.form:
+        if finish_request_form.validate_on_submit():
+            request_id = finish_request_form.request_id.data
+            valid, output = chaincode(["updateRequest", request_id, "Request Implemented","implemented"])
+            if valid:
+                flash("Request Completed!", "success")
+                return redirect(url_for('admin_display_requests'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
+                return redirect(url_for('admin_display_requests'))
+    elif request.method == "POST" and 'put_on_hold' in request.form:
+        if hold_request_form.validate_on_submit():
+            request_id = hold_request_form.request_id.data
+            valid, output = chaincode(["updateRequest", request_id, "Request On Hold","on hold"])
+            if valid:
+                flash("Request On Hold!", "success")
+                return redirect(url_for('admin_display_requests'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
+                return redirect(url_for('admin_display_requests'))
+    elif request.method == "POST" and 'resume' in request.form:
+        if resume_request_form.validate_on_submit():
+            request_id = resume_request_form.request_id.data
+            valid, output = chaincode(["updateRequest", request_id, "Request Resumed","in progress"])
+            if valid:
+                flash("Request Resumed!", "success")
+                return redirect(url_for('admin_display_requests'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
+                return redirect(url_for('admin_display_requests'))
+    elif request.method == "POST" and 'drop' in request.form:
+        if drop_request_form.validate_on_submit():
+            request_id = drop_request_form.request_id.data
+            valid, output = chaincode(["updateRequest", request_id, "Request Dropped","dropped"])
+            if valid:
+                flash("Request Dropped!", "success")
+                return redirect(url_for('admin_display_requests'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
+                return redirect(url_for('admin_display_requests'))
+
+    return render_template("admin_display_requests.html", requests = requests,initiate_request_form = initiate_request_form, finish_request_form = finish_request_form, hold_request_form = hold_request_form, resume_request_form = resume_request_form, drop_request_form = drop_request_form)
+
+
+@app.route("/add_request", methods = ['GET', 'POST'])
+@login_required
+def add_request():
+    add_request_form = AddRequestForm()
     if request.method == 'POST':
-        if admin_login_form.validate_on_submit():
-            return render_template("admin_login.html",form=admin_login_form)
-    return render_template("admin_login.html",form=admin_login_form)
+        if add_request_form.validate_on_submit():
+            description = add_request_form.description.data
+            valid, output = chaincode(["addRequest", description, session['type']])
+            if valid:
+                flash("Request Added Successfully", "success")
+                return redirect(url_for('add_request'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
+                return redirect(url_for('add_request'))
+    return render_template("add_request.html",form=add_request_form)
 
-@app.route("/student_feedbacks", methods = ['GET', 'POST'])
-def student_feedbacks():
+@app.route("/display_requests", methods = ['GET', 'POST'])
+@login_required
+def display_requests():
+    confirm_request_form = ConfirmRequestForm()
+    view_history_form = ViewHistoryForm()
+    filter_confirmed_requests_form = FilterConfirmedRequestsForm()
+    filter_unconfirmed_requests_form = FilterUnconfirmedRequestsForm()
+
+    if request.method == 'POST' and 'confirmed_requests' in request.form:
+        if filter_confirmed_requests_form.validate_on_submit():
+            valid, requests = chaincode(["displayRequests", session['type'], 'confirmed'])
+            return render_template("display_requests.html", requests = requests, filter_confirmed_requests_form = filter_confirmed_requests_form, filter_unconfirmed_requests_form = filter_unconfirmed_requests_form, view_history_form = view_history_form, confirm_request_form = confirm_request_form)
+
+    if request.method == 'POST' and 'unconfirmed_requests' in request.form:
+        if filter_unconfirmed_requests_form.validate_on_submit():
+            valid, requests = chaincode(["displayRequests", session['type'], 'unconfirmed'])
+            return render_template("display_requests.html", requests = requests, filter_confirmed_requests_form = filter_confirmed_requests_form, filter_unconfirmed_requests_form = filter_unconfirmed_requests_form, view_history_form = view_history_form, confirm_request_form = confirm_request_form)
+
+    valid, requests = chaincode(["displayRequests", session['type'], 'all'])
+
+    #Assuming requests is a list of dictionaries
+    return render_template("display_requests.html", requests = requests, filter_confirmed_requests_form = filter_confirmed_requests_form, filter_unconfirmed_requests_form = filter_unconfirmed_requests_form, view_history_form = view_history_form, confirm_request_form = confirm_request_form)
     
+@app.route('/confirm_request', methods = ['POST'])
+def confirm_request():
+    confirm_request_form = ConfirmRequestForm()
+    if request.method == 'POST' and 'confirm' in request.form:
+        if confirm_request_form.validate_on_submit():
+            email_id = confirm_request_form.email_id.data
+            request_id = confirm_request_form.request_id.data
+            name = confirm_request_form.name.data
+            valid, output = chaincode(["confirmRequest", request_id, email_id,name])
+            if valid:
+                flash("Request Confirmed!", "success")
+                return redirect(url_for('display_requests'))
+            else:
+                flash("Something went wrong. Please try again.", "danger")
+                return redirect(url_for('display_requests'))
+            
+@app.route('/view_history', methods = ['POST'])
+def view_history():
+    view_history_form = ViewHistoryForm()
+    if request.method == 'POST':
+        if view_history_form.validate_on_submit():
+            request_id = view_history_form.request_id.data
+            valid, history = chaincode(['queryRequestHistory', request_id])
+            return render_template("view_history.html", history = history)
 
-# ########## AUTHENTICATION ##########
-# @app.get("/signup")
-# def get_signup():
-#     """
-#     Signup page
-#     """
+@app.route("/admin_logout")
+def admin_logout():
+    if 'admin_password' in session:
+        session.pop('admin_password')
+        flash("Logged out successfully", "success")
+        return redirect(url_for("home"))
+    else:
+        flash("You have to be an admin to access this!", "warning")
+        return redirect(url_for("home"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.", "success")
+    return redirect(url_for('home'))
 
 
-# @app.post("/signup")
-# def post_signup():
-#     """
-#     Process signup details
-#     """
-
-
-# @app.get("/signin")
-# def get_signin():
-#     """
-#     Signin page
-#     """
-
-
-# @app.post("/signin")
-# def post_signin():
-#     """
-#     Process signin details
-#     """
-
-
-# @app.post("/signout")
-# def signout():
-#     """
-#     Process user signout
-#     """
 if __name__ == '__main__':
-    app.run(debug=True, port = 5001)
+    app.run(debug=True)
--- flask/forms.py
+++ flask/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import NumberRange,DataRequired, EqualTo, Email,Length, Regexp
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, FloatField, FileField, DateField, IntegerField, HiddenField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, FloatField, FileField, DateField, IntegerField, HiddenField, TextAreaField
 from wtforms_sqlalchemy.fields import QuerySelectField
 # from .models import *
 # from flask_uploads import UploadSet, IMAGES
@@ -14,80 +14,58 @@ class RegistrationForm(FlaskForm):
     ashokaid = name = StringField('Ashoka ID', validators=[DataRequired(), Regexp('^\d{10}$', message='Ashoka ID must be 10 digits')], render_kw={"placeholder": "Enter Ashoka ID"})
     email_id = StringField('Email', validators=[DataRequired(), Email(message='Please enter a valid email')], render_kw={"placeholder": "Enter email"})
     name = StringField('Name', validators=[DataRequired(), Length(min = 3, max = 50)], render_kw={"placeholder": "Enter Name"})
-    role = SelectField('Role', choices=[('faculty', 'Faculty'), ('student', 'Student')], validators=[DataRequired()])
+    password = PasswordField('Password', validators= [DataRequired()], render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField('Confirm Password', validators= [DataRequired(), EqualTo('password')], render_kw={"placeholder": "Confirm Password"})
+    type = SelectField('Type', choices=[('faculty', 'Faculty'), ('student', 'Student')], validators=[DataRequired()])
     submit = SubmitField('Sign Up!')
 
 class LoginForm(FlaskForm):
     email_id = StringField('Email', validators=[DataRequired(), Email(message='Please enter a valid email')], render_kw={"placeholder": "Enter email"})
     password = PasswordField('Password', validators= [DataRequired()], render_kw={"placeholder": "Password"})
-    role = SelectField('Role', choices=[('faculty', 'Faculty'), ('student', 'Student')], validators=[DataRequired()])
-    submit = SubmitField('Sign In!')
+    login = SubmitField('Sign In!')
 
-class CreateCategoryForm(FlaskForm):
-    category = StringField('Category', validators=[DataRequired(), Length(min = 2, max = 50)], render_kw={"placeholder": "Enter a new category"})
-    save = SubmitField('Save')
+class AddRequestForm(FlaskForm):
+    description = TextAreaField('Description', validators=[DataRequired(), Length(min = 5, max = 100)], render_kw={"placeholder": "Enter Request"})
+    submit = SubmitField('Submit')
 
-class RemoveCategoryForm(FlaskForm):
-    category = QuerySelectField('Select a Category',query_factory=lambda: Category.query,get_label='name', validators=[DataRequired()], render_kw={"placeholder": "Select a Category!"})
-    save = SubmitField('Save')
+class ConfirmRequestForm(FlaskForm):
+    request_id = HiddenField("Request Id", validators=[DataRequired()])
+    confirm = SubmitField('Confirm')
 
-class AddUnitForm(FlaskForm):
-    unit = StringField('Unit', validators=[DataRequired(), Length(min = 1, max = 20)], render_kw={"placeholder": "Enter a new unit"})
-    save = SubmitField('Save')
+class FilterConfirmedRequestsForm(FlaskForm):
+    confirmed_requests = SubmitField('Confirmed Requests')
 
-class RemoveUnitForm(FlaskForm):
-    unit = QuerySelectField('Select a Unit',query_factory=lambda: Unit.query,get_label='unit', validators=[DataRequired()], render_kw={"placeholder": "Select a Unit!"})
-    save = SubmitField('Save')
+class FilterUnconfirmedRequestsForm(FlaskForm):
+    unconfirmed_requests = SubmitField('Unconfirmed Requests')
 
-class AddProductForm(FlaskForm):
-    category = QuerySelectField('Select a Category',query_factory=lambda: Category.query,get_label='name', validators=[DataRequired()], render_kw={"placeholder": "Select a Category!"})
-    product_name = StringField('Product Name', validators=[DataRequired(), Length(min = 3, max = 50)], render_kw={"placeholder": "Enter Product Name"})
-    unit = SelectField('Unit', validators=[DataRequired()], render_kw={"placeholder": "Select Unit"})
-    rate_per_unit = FloatField('Rate per unit', validators=[DataRequired(), NumberRange(message="Please enter a non negative price",min=0)], render_kw={"placeholder": "Enter Rate Per Unit"})
-    quantity = IntegerField('Quantity', validators = [DataRequired(),NumberRange(message="Please enter a valid quantity",min=1)], render_kw={"placeholder": "Enter Quantity in Stock"})
-    # image = FileField('Image', validators=[FileAllowed(images, 'Images only!')], render_kw = {"placeholder": "Select an Image"})
-    save = SubmitField('Save')
+class ViewHistoryForm(FlaskForm):
+    request_id = HiddenField("Request Id", validators=[DataRequired()])
+    view_history = SubmitField('View History')
 
-class UpdateProductForm(FlaskForm):
-    product_name = StringField('Product Name', validators=[DataRequired(), Length(min = 3, max = 50)], render_kw={"placeholder": "Enter Product Name"})
-    unit = SelectField('Unit', validators=[DataRequired()], render_kw={"placeholder": "Select Unit"})
-    rate_per_unit = FloatField('Rate per unit', validators=[DataRequired(),NumberRange(message="Please enter a non negative price",min=0)], render_kw={"placeholder": "Enter Rate Per Unit"})
-    quantity = IntegerField('Quantity', validators = [DataRequired(),NumberRange(message="Please enter a valid quantity",min=1)], render_kw={"placeholder": "Enter Quantity in Stock"})
-    # image = FileField('Image', validators=[FileAllowed(images, 'Images only!')], render_kw = {"placeholder": "Select an Image"})
-    save = SubmitField('Save')
+class AcceptUserRegistrationRequestForm(FlaskForm):
+    email_id = HiddenField("Email Id", validators=[DataRequired()])
+    accept = SubmitField('Accept')
 
-class RemoveProductForm(FlaskForm):
-    product_name = QuerySelectField('Select a Product',query_factory=lambda: Products.query,get_label='name', validators=[DataRequired()], render_kw={"placeholder": "Select a product to remove!"})
-    unit = QuerySelectField('Select a Unit',query_factory=lambda: Unit.query,get_label='unit', validators=[DataRequired()], render_kw={"placeholder": "Select a unit to remove!"})
-    save = SubmitField('Save')
+class RejectUserRegistrationRequestForm(FlaskForm):
+    email_id = HiddenField("Email Id", validators=[DataRequired()])
+    reject = SubmitField('Reject')
 
-# class RemoveProductUnitForm(FlaskForm):
-#     name = HiddenField('Name', validators=[DataRequired()])
-#     # unit = QuerySelectField('Select a Product Variant',query_factory=lambda:Products.query, get_label='unit', validators=[DataRequired()], render_kw={"placeholder": "Select which product variant to remove!"})
-#     unit = SelectField('Unit', validators=[DataRequired()])
-#     save = SubmitField('Save')
+class InitiateRequestForm(FlaskForm):
+    request_id = HiddenField("Request Id", validators=[DataRequired()])
+    initiate = SubmitField('Initiate')
 
-class AddToCartForm(FlaskForm):
-    id = HiddenField("Id", validators=[DataRequired()])
-    product_name = HiddenField("Product Name", validators=[DataRequired()])
-    quantity = IntegerField("Quantity",validators=[DataRequired('Choose some quantity'), NumberRange(message = "Please enter a valid quantity",min=1, max =6)], render_kw={"placeholder": "Enter Number of Products"}, default = None)
-    add_to_cart = SubmitField('Add To Cart')
+class HoldRequestForm(FlaskForm):
+    request_id = HiddenField("Request Id", validators=[DataRequired()])
+    put_on_hold = SubmitField('Put On Hold')
 
-class SearchForm(FlaskForm):
-    search_query = StringField('Product Name', validators=[DataRequired(), Length(min = 3, max = 30)], render_kw={"placeholder": "Search for products"})
-    search = SubmitField('Search')
+class ResumeRequestForm(FlaskForm):
+    request_id = HiddenField("Request Id", validators=[DataRequired()])
+    resume = SubmitField('Resume')
 
-class AddOneCartForm(FlaskForm):
-    id = HiddenField("Id", validators=[DataRequired()])
-    add = SubmitField('+')
+class FinishRequestForm(FlaskForm):
+    request_id = HiddenField("Request Id", validators=[DataRequired()])
+    finish = SubmitField('Finish')
 
-class RemoveOneCartForm(FlaskForm):
-    id = HiddenField("Id", validators=[DataRequired()])
-    remove = SubmitField('-')
-
-class RemoveAllCartForm(FlaskForm):
-    id = HiddenField("Id", validators=[DataRequired()])
-    remove_all = SubmitField('Remove')
-
-class CheckoutForm(FlaskForm):
-    checkout = SubmitField('Checkout')
+class DropRequestForm(FlaskForm):
+    request_id = HiddenField("Request Id", validators=[DataRequired()])
+    drop = SubmitField('Drop')
